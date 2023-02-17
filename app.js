f0273bd9a2700c00ef36cf40c4ade33aa5caf796
LinTzuAn <a22341254@gmail.com>
2023-02-17 08:49:18 +0800

    feat: add register function

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const session = require('express-session')
 const exphbs = require('express-handlebars')
+const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
 const router = require('./routes')
 const usePassport = require('./config/passport')
@@ -17,6 +18,7 @@ app.use(session({
   saveUninitialized: true
 }))
 
+app.use(bodyParser.urlencoded({ extended: true }))
 app.use(router)
 
 app.use(methodOverride('_method'))
--- routes/modules/users.js
+++ routes/modules/users.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const passport = require('passport')
+const User = require('../../models/user')
 
 router.get('/login', (req, res) => {
   res.render('login')
@@ -10,5 +11,50 @@ router.post('/login', passport.authenticate('local', {
     successRedirect: '/',
     failureRedirect: '/users/login'
 }))
+
+router.get('/register', (req, res) => {
+  res.render('register')
+})
+
+router.post('/register', async (req, res) => {
+  try {
+    const { name, email, password, confirmPassword } = req.body
+    const errors = []
+    if (!name || !email || !password || !confirmPassword) {
+      errors.push({ message: 'Please fill in all the blanks.' })
+    }
+    if (password !== confirmPassword) {
+      errors.push({ message: 'Passwords do not match.' })
+    }
+    if (errors.length) {
+      return res.render('register', {
+        errors,
+        name,
+        email,
+        password,
+        confirmPassword
+      })
+    }
+    const user = await User.findOne({ email })
+    if (user) {
+      errors.push({ message: 'This email address has been taken.' })
+      return res.render('register', {
+        errors,
+        name,
+        email,
+        password,
+        confirmPassword
+      })
+    }
+    await User.create({
+      name,
+      email,
+      password,
+    })
+    res.redirect('/users/login')
+  } catch (err) {
+    console.log(err)
+  }
+})
 
 module.exports = router
