b244a551268dba3c74784c8c02884b11a63bcf11
LinTzuAn <a22341254@gmail.com>
2023-02-18 20:02:11 +0800

    chore: heroku init

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ if (process.env.NODE_ENV !== 'production') {
 }
 
 const PORT = process.env.PORT || 3000
+
 const routes = require('./routes')
 const usePassport = require('./config/passport')
 require('./config/mongoose')
